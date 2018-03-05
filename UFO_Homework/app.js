4c4266d8964e3c38e04040241a2824e5fa1a43e9
Christiaan Van den Akker <christiaan.vda@gmail.com>
2018-03-04 23:57:37 -0800

    Add files via upload

---
+++ UFO_Homework/app.js
@@ -0,0 +1,29 @@
+// Get references to the tbody element, input field and button
+var $tbody = document.querySelector("tbody");
+var $dateInput = document.querySelector("#datetime");
+var $searchBtn = document.querySelector("#search");
+
+// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+// Set filteredSightings to sightingsData initially
+var filteredSightings = sightingsData;
+
+// renderTable renders the filteredSightings to the tbody
+function renderTable() {
+  $tbody.innerHTML = "";
+  for (var i = 0; i < filteredSightings.length; i++) {
+    // Get get the current sightings object and its fields
+    var sighting = filteredSightings[i];
+    var fields = Object.keys(sighting);
+    // Create a new row in the tbody, set the index to be i + startingIndex
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      // For every field in the sighting object, create a new cell and set its inner text to be the current value at the current sighting's field
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = sighting[field];
+    }
+  }
+}
+
